b723d768f6b93d1d708d5af53b8c9b327d2d26c6
shubham <shubhamrnps@gmail.com>
2025-05-11 18:07:41 +0530

    test

--- src/pages/portswigger-labs/docsContents.ts
+++ src/pages/portswigger-labs/docsContents.ts
@@ -5,31 +5,9 @@ export const docs: CardListData = {
   title: 'portswigger-labs content',
   list: [
     {
-      title: 'Setup',
+      title: 'API Testing',
       children: [
-        { title: 'Getting Started', link: '/portswigger-labs/setup/getting-started' },
-        { title: 'Configuration', link: '/portswigger-labs/setup/configuration' },
-        { title: 'Authoring Content', link: '/portswigger-labs/setup/content' },
-        { title: 'Deployment', link: '/portswigger-labs/setup/deployment' }
-      ]
-    },
-    {
-      title: 'Integrations',
-      children: [
-        { title: 'Comment System', link: '/portswigger-labs/integrations/comment' },
-        { title: 'Friend Links', link: '/portswigger-labs/integrations/links' },
-        { title: 'Shiki Code', link: '/portswigger-labs/integrations/code' },
-        { title: 'User Components', link: '/portswigger-labs/integrations/components' },
-        { title: 'Advanced Components', link: '/portswigger-labs/integrations/advanced' },
-        { title: 'Other Integrations', link: '/portswigger-labs/integrations/others' }
-      ]
-    },
-    {
-      title: 'Advanced',
-      children: [
-        { title: 'Update Theme', link: '/portswigger-labs/advanced/update' },
-        { title: 'Optimize Your Site', link: '/portswigger-labs/advanced/optimize' },
-        { title: 'Acknowledgements', link: '/portswigger-labs/advanced/thanks' }
+        { title: 'Exploiting server-side parameter pollution in a REST URL', link: '/portswigger-labs/api/api5' }
       ]
     }
   ]
