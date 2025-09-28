f3c8b3ae23775584e1d6a7767a281cc5b6a92292
Shubham Ranpise <115131316+ShubhamNR007@users.noreply.github.com>
2025-09-28 20:18:38 +0530

    xss 10

--- src/pages/portswigger-labs/docsContents.ts
+++ src/pages/portswigger-labs/docsContents.ts
@@ -55,7 +55,8 @@ export const docs: CardListData = {
         { title: 'DOM XSS in jQuery selector sink using a hashchange event', link: '/portswigger-labs/xss/xss6' },
         { title: 'Reflected XSS into attribute with angle brackets HTML-encoded', link: '/portswigger-labs/xss/xss7' },
         { title: 'Stored XSS into anchor href attribute with double quotes HTML-encoded', link: '/portswigger-labs/xss/xss8' },
-        { title: 'Reflected XSS into a JavaScript string with angle brackets HTML encoded', link: '/portswigger-labs/xss/xss9' }
+        { title: 'Reflected XSS into a JavaScript string with angle brackets HTML encoded', link: '/portswigger-labs/xss/xss9' },
+        { title: 'DOM XSS in document.write sink using source location.search inside a select element', link: '/portswigger-labs/xss/xss10' }
       ]
     }
   ]
